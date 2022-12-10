17300d0b6536f1d649f9d4d1cc05b0f04ef09b71
Tejas Agrahari <tejasflying@gmail.com>
2022-12-10 13:46:30 +0530

    Updated Files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true,      //Environment Setup
   },
   extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
